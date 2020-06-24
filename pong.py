0d484c11270078c38b25a8883411f0b71e5a7541
MikeChunko <chunkomike@gmail.com>
2020-06-24 11:14:16 -0400

    nn frontend

--- pong.py
+++ pong.py
@@ -1,11 +1,31 @@
 # Implementation of Pong in pygame
+import numpy as np
 import pygame as pyg
 import random
 import math
+import argparse
+from pong_dqn import Agent
+from random import randrange, randint
+from keras.utils import to_categorical
+
+
+def define_parameters():
+    params = dict()
+    params["epsilon_decay_linear"] = 1 / 500
+    params["learning_rate"] = 0.0005
+    params["first_layer_size"] = 15
+    params["second_layer_size"] = 15
+    params["third_layer_size"] = 15
+    params["episodes"] = 1000
+    params["memory_size"] = 2500
+    params["batch_size"] = 50
+    params["weights_path"] = "weights.hdf5"
+    params["train"] = True
+    return params
 
 
 class Pong:
-    def __init__(self, screen_x=300, screen_y=300):
+    def __init__(self, draw=True, screen_x=300, screen_y=300):
         pyg.init()
         pyg.font.init()
         self.screen_x, self.screen_y = screen_x, screen_y
@@ -17,6 +37,8 @@ class Pong:
         self.clock = pyg.time.Clock()
         self.sentinel = True
         self.font = pyg.font.SysFont("hack", 30)
+        self.draw = draw
+        self.hit = False
 
         # _1 is for the player 1, _2 is for player 2
         self.score_1 = self.score_2 = 0
@@ -48,40 +70,35 @@ class Pong:
             self.screen.blit(textsurface, textsurface.get_rect(center=(self.screen_x // 2, self.size_y * 4)))
             self.score_timer -= 1
 
-    def process_input(self):
+    def get_features(self):
+        return [
+            self.paddle_2 +  (self.paddle_length * self.size_y / 2),
+            self.ball_y, self.ball_delta_x * self.delta_factor,
+            self.ball_delta_y * self.delta_factor
+        ]
+
+    def process_input(self, input):
         """ Fetch and handle user input. """
         for event in pyg.event.get():  # Quit
             if event.type == pyg.QUIT:
                 pyg.quit()
                 quit()
 
-            # Player 1
-            if event.type == pyg.KEYUP:  # Move release
-                if event.key == pyg.K_w and self.delta_1 < 0:
-                    self.delta_1 = 0
-                elif event.key == pyg.K_s and self.delta_1 > 0:
-                    self.delta_1 = 0
-            elif event.type == pyg.KEYDOWN:  # Move begin
-                if event.key == pyg.K_w:
-                    self.delta_1 = -.5
-                elif event.key == pyg.K_s:
-                    self.delta_1 = .5
-
-            # Player 2
-            if event.type == pyg.KEYUP:  # Move release
-                if event.key == pyg.K_UP and self.delta_2 < 0:
-                    self.delta_2 = 0
-                elif event.key == pyg.K_DOWN and self.delta_2 > 0:
-                    self.delta_2 = 0
-            elif event.type == pyg.KEYDOWN:  # Move begin
-                if event.key == pyg.K_UP:
-                    self.delta_2 = -.5
-                elif event.key == pyg.K_DOWN:
-                    self.delta_2 = .5
-
             if event.type == pyg.KEYDOWN and event.key == pyg.K_r:  # Restart
                 self.sentinel = False
                 self.score_1 = self.score_2 = 0
+
+        # Player 1
+        if self.paddle_1 > self.ball_y:
+            self.delta_1 = -.5
+        else:
+            self.delta_1 = .5
+
+        # Player 2
+        if input == 0:
+            self.delta_2 = -.5
+        elif input == 1:
+            self.delta_2 = .5
 
     def move_paddles(self):
         """ Move both paddles.
@@ -114,7 +131,8 @@ class Pong:
             """ Speed up the ball. """
             if self.delta_factor > self.min_delta_factor:
                 self.delta_factor -= self.delta_factor / 12
-            print(self.delta_factor)
+
+        self.hit = False
 
         # Check paddle intersect
         self.ball_x += self.ball_delta_x
@@ -139,6 +157,7 @@ class Pong:
             if intersect_angle > -1.1 and intersect_angle < 1.1:  # Paddle miss
                 self.ball_delta_x, self.ball_delta_y = -math.cos(intersect_angle) / self.delta_factor, \
                                                        -math.sin(intersect_angle) / self.delta_factor
+            self.hit = True
         elif self.ball_x > self.screen_x + self.size_x:
             self.score_1 += 1
             self.sentinel = False
@@ -148,9 +167,9 @@ class Pong:
         if self.ball_y <= self.size_y or self.ball_y >= self.screen_y - 2 * self.size_y:
             self.ball_delta_y = -self.ball_delta_y
 
-    def step(self, tick=200):
+    def step(self, tick=200, input=0):
         """ Process one step of the game. """
-        self.process_input()
+        self.process_input(input)
         self.move_paddles()
         self.move_ball()
         self.display()
@@ -158,11 +177,87 @@ class Pong:
         self.clock.tick(tick)
 
 
+def str_to_bool(v):
+    if isinstance(v, bool):
+        return v
+    if v.lower() in ("yes", "true", "t", "y", "1"):
+        return True
+    elif v.lower() in ("no", "false", "f", "n", "0"):
+        return False
+    else:
+        raise argparse.ArgumentTypeError("Boolean value expected.")
+
+
 if __name__ == "__main__":
+    pyg.init()
+    pyg.font.init()
+    tick = 1000
+    params = define_parameters()
+    num_games = 0
+
+    parse = argparse.ArgumentParser()
+    parse.add_argument("--draw", type=str_to_bool, default=True, help="whether or not to draw the game")
+    parse.add_argument("--train", type=str_to_bool, default=True, help="whether or not to train")
+    args = parse.parse_args()
+    params["train"] = args.train
+
+    agent = Agent(params)
     score_1 = score_2 = 0
-    while True:
-        game = Pong()
+
+    while num_games < params["episodes"]:
+        game = Pong(draw=args.draw)
         game.score_1, game.score_2 = score_1, score_2
+        timer = 0
+
         while game.sentinel:
-            game.step()
+            if timer == 0:
+                if not params["train"]:
+                    agent.epsilon = 0
+                else:
+                    # Epsilon determines randomness factor
+                    agent.epsilon = 1 - (num_games * params["epsilon_decay_linear"])
+
+                state_old = np.asarray(game.get_features())
+                if randint(0, 1) < agent.epsilon:  # Random action
+                    final_input = randint(0, 1)
+                    final_move = to_categorical(final_input, num_classes=2)
+                else:  # Predict action based on nn
+                    prediction = agent.model.predict(state_old.reshape((1, 4)))
+                    final_move = to_categorical(np.argmax(prediction[0]), num_classes=2)
+
+                    if np.array_equal(final_move, [1, 0]):
+                        final_input = 0
+                    else:
+                        final_input = 1
+
+            game.step(tick=tick, input=final_input)
+
+            if timer == 0:
+                timer = 100
+                state_new = np.asarray(game.get_features())
+                reward = (score_1 - game.score_1) * 100
+                if reward == 0:
+                    reward += 1
+                if game.hit:
+                    reward += 500
+
+                if params["train"]:
+                    # Train short memory based on the new action and state
+                    agent.train_short_memory(state_old, final_move, reward, state_new, not game.sentinel)
+                    # Store into long term memory
+                    agent.remember(state_old, final_move, reward, state_new, not game.sentinel)
+            else:
+                timer -= 1
+
             score_1, score_2 = game.score_1, game.score_2
+
+        num_games += 1
+        print("Round: {}, Score: {} - {}".format(num_games, score_1, score_2))
+
+        if params["train"]:
+            agent.replay_new(params["batch_size"])
+
+    # Save the calculated weights for later use
+    if params["train"]:
+        agent.model.save_weights(params["weights_path"])
+
